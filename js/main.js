44ec3f6a327b0bd623500b7f0a4cc16d865aecb7
estevan-ulian <estevan.ulian@gmail.com>
2022-05-20 14:31:26 -0300

    Update main.js
    Printing objects in alphabetical order of countries name_ptbr.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 const API = 'https://api.sellead.com/api/v1/country';
 
+function createListInHTML (value) {
+    const listHTML = document.querySelector('.country-list');
+
+    const html = `
+    <div class="item">
+        <span>${value.name_ptbr}</span>
+        <span>|</span>
+        <span>${value.code}</span>
+    </div>
+    `
+
+    return listHTML.insertAdjacentHTML('afterend', html);
+};
+
 const countries = fetch(API)
     .then(res => {
 
@@ -13,20 +27,42 @@ const countries = fetch(API)
 
     })
     .then(data => {
-        const html = data
-            .map(v => {
-                return (`
-                    <p class="item">
-                        <span>${v.code}</span>
-                        <span>|</span>
-                        <span>${v.name_ptbr}</span>
-                    </p>
-                `);
-            })
-            .join('');
+        const dataSort = data.sort( (a, b) => {
 
-        const countryList = document.querySelector('.country-list');
-        countryList.insertAdjacentHTML(
-            'afterend', html);
+            let nameA = a.name_ptbr.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+            let nameB = b.name_ptbr.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
+            if (nameA > nameB) return -1;
+            if (nameA < nameB) return 1;
+            return 0;
+
+        });
+
+        dataSort.filter(result => {
+
+            createListInHTML(result)
+        
+        });  
+
     })
-    .catch(error => console.log('ERROR -> dados não encontrados.'));
+    .catch(err => console.log(`ERRO! Não foi possível exibir os dados. ${err}`));
+
+
+    // .then(data => {
+    //     const html = data
+    //         .map(v => {
+    //             return (`
+    //                 <p class="item">
+    //                     <span>${v.code}</span>
+    //                     <span>|</span>
+    //                     <span>${v.name_ptbr}</span>
+    //                 </p>
+    //             `);
+    //         })
+    //         .join('');
+
+    //     const countryList = document.querySelector('.country-list');
+    //     countryList.insertAdjacentHTML(
+    //         'afterend', html);
+    // })
+    // .catch(error => console.log('ERROR -> dados não encontrados.'));
